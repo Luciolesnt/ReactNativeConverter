f864d03a8fce704732575cd8b6cc21d31b899757
Luciolesnt <luciesanter@gmail.com>
2021-04-03 17:22:56 +0200

    learning/fetched data with local API

--- App/index.js
+++ App/index.js
@@ -1,13 +1,4 @@
 import React from 'react';
 import Navigation from './config/Navigation';
-import { api } from './util/api';
-
-api()
-.then(response => {
-    console.log('response', response);
-})
-.catch(error => {
-    console.log('error', error);
-})
 
 export default () => <Navigation />;
--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,10 +1,29 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from "react";
+import { api } from "./api";
 
 export const ConversionContext = createContext();
 
+const DEFAULT_BASE_CURRENCY = 'USD';
+const DEFAULT_QUOTE_CURRENCY = 'GBP';
+
 export const ConversionContextProvider = ({ children }) => {
-  const [baseCurrency, setBaseCurrency] = useState("USD");
-  const [quoteCurrency, setQuoteCurrency] = useState("GBP");
+  const [baseCurrency, _setBaseCurrency] = useState(DEFAULT_BASE_CURRENCY);
+  const [quoteCurrency, setQuoteCurrency] = useState(DEFAULT_QUOTE_CURRENCY);
+  const [date, setDate] = useState();
+  const [rates, setRates] = useState({});
+
+  const setBaseCurrency = (currency) => {
+    return api(`/latest?base=${currency}`)
+      .then((response) => {
+        console.log(response);
+        _setBaseCurrency(currency);
+        setDate(response.date);
+        setRates(response.rates)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const swapCurrencies = () => {
     setBaseCurrency(quoteCurrency);
@@ -17,12 +36,18 @@ export const ConversionContextProvider = ({ children }) => {
     swapCurrencies,
     setBaseCurrency,
     setQuoteCurrency,
-  }
+    date,
+    rates,
+  };
+
+  useEffect(() => {
+    setBaseCurrency(DEFAULT_BASE_CURRENCY);
+    setQuoteCurrency(DEFAULT_QUOTE_CURRENCY);
+  }, [])
 
   return (
     <ConversionContext.Provider value={contextValue}>
       {children}
     </ConversionContext.Provider>
- )
-}
-
+  );
+};
